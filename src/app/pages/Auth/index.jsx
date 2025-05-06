2cf7d275e1f234d7fc31e1ba4c659a3780a6aa83
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-05-06 13:07:43 +0530

    Update index.jsx

--- src/app/pages/Auth/index.jsx
+++ src/app/pages/Auth/index.jsx
@@ -23,8 +23,8 @@ export default function SignIn() {
   } = useForm({
     resolver: yupResolver(schema),
     defaultValues: {
-      username: "username",
-      password: "password",
+      username: "",
+      password: "",
     },
   });
 
